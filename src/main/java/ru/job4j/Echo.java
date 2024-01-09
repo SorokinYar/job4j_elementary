e161668914cf2f8847ed9f9d1c5a51f1b91eb7e8
SorokinYar <Sorokin.yar@yandex.ru>
2024-01-09 22:20:24 +0100

    4.1. Разбор первой программы. [#159033 #22363]

--- src/main/java/ru/job4j/Echo.java
+++ src/main/java/ru/job4j/Echo.java
@@ -1,4 +1,13 @@
 package ru.job4j;
 
 public class Echo {
+    public static void main(String[] args) {
+        System.out.println("Privet");
+        sum(12,4);
+    }
+
+    public static void sum(int a, int b) {
+        int sum = a+b;
+        System.out.println(sum);
+    }
 }
---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Sorokin Yaroslav");
+        System.out.println("02.03.1996");
+    }
+}
