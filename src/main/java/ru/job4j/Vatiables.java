61738a2a52de27175b699a8bc5e63c8960829da5
SorokinYar <Sorokin.yar@yandex.ru>
2024-01-09 23:05:16 +0100

    0. Что такое переменная. [#3610 #22376]

---
+++ src/main/java/ru/job4j/Vatiables.java
@@ -0,0 +1,13 @@
+package ru.job4j;
+
+public class Vatiables {
+    public static void main(String[] args) {
+        int age = 18;
+        float length = 78.0332F;
+        boolean baby = true;
+        System.out.println(age);
+        System.out.println(length);
+        System.out.println(baby);
+
+    }
+}
