0414a701dcef1c16709429fac632d3b04accd89b
SorokinYar <Sorokin.yar@yandex.ru>
2024-01-09 23:28:27 +0100

    2. Переменные. [#172635 #22366]

---
+++ src/main/java/ru/job4j/ListOfVariables.java
@@ -0,0 +1,19 @@
+package ru.job4j;
+
+public class ListOfVariables {
+    public static void main(String[] args) {
+        byte age = 27;
+        char gender = 'M';
+        float height = 1.78F;
+        int brick = 28922;
+        long bacteria = 29875298;
+        long space = 298752984;
+        boolean job = true;
+        short file = 128;
+        System.out.println("Age = " + age);
+        System.out.println("Gender = " + gender);
+        System.out.println("Height = " + height);
+        System.out.println("Brick = " + brick);
+
+    }
+}
