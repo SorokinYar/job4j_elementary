2069332583eda94ad41852b3eb3af7fce4a0b348
SorokinYar <Sorokin.yar@yandex.ru>
2024-01-10 16:01:52 +0100

    4.2. Результат работы метода [#232622 #22368]

---
+++ src/main/java/ru/job4j/calculator/MathFunction.java
@@ -0,0 +1,18 @@
+package ru.job4j.calculator;
+
+public class MathFunction {
+    public static int func1(int x) {
+        int y = x * x + 1;
+        return y;
+    }
+    public static int func2(int x) {
+        int y = 1 / x;
+        return y;
+    }
+
+    public static void main(String[] args) {
+        int results3 = MathFunction.func1(100);
+        System.out.println(results3);
+
+    }
+}
