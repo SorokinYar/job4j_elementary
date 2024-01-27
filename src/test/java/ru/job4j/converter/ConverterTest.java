21ad92115e658b1d321a103673641873cd825108
SorokinYar <Sorokin.yar@yandex.ru>
2024-01-28 00:38:08 +0100

    1. Модульные тесты. [#159435 #22393]

---
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.converter;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.withPrecision;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConverterTest {
+
+    @Test
+    public void rubleToEuroTest() {
+        float input = 140;
+        float expected = 2;
+        float output = Converter.rubleToEuro(input);
+        float value = 0.0001f;
+        assertThat(output).isEqualTo(expected, withPrecision(value));
+    }
+
+    @Test
+    public void rubleToDollarTest() {
+        int input = 540;
+        float expected = 9;
+        float output = Converter.rubleToDollar(input);
+        float value = 0.0001f;
+        assertThat(output).isEqualTo(expected, withPrecision(value));
+    }
+
+}
