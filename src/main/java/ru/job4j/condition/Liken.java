52fdcf8cb17e334a9ea198b3d9b387bcdc9cf08d
SorokinYar <Sorokin.yar@yandex.ru>
2024-01-31 10:19:18 +0100

    0. Операции сравнения [#235631 #22351]

---
+++ src/main/java/ru/job4j/condition/Liken.java
@@ -0,0 +1,26 @@
+package ru.job4j.condition;
+
+public class Liken {
+    public static void main (String[] args) { //Почему стринг , квадратные скобки и что такое args&
+        int first = 10;
+        int second = 9;
+
+        boolean result = first > second;
+        System.out.println(result);
+
+        result = first < second; //Не ставлю тип переменной так это значит что я назначаю новую переменную, а я уже ее обьявил на 8 строке?
+        System.out.println(result);
+
+        result = first == second;
+        System.out.println(result);
+
+        result = first >= second;
+        System.out.println(result);
+
+        result = first <= second;
+        System.out.println(result);
+
+        result = first != second;
+        System.out.println(result);
+    }
+}
