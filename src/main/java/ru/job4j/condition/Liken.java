174d1550dd718e5d295e25b1b5ed99b9c15c1eda
SorokinYar <Sorokin.yar@yandex.ru>
2024-01-31 10:24:12 +0100

    0. Операции сравнения [#235631 #22351]

--- src/main/java/ru/job4j/condition/Liken.java
+++ src/main/java/ru/job4j/condition/Liken.java
@@ -5,7 +5,7 @@ public class Liken {
         int first = 10;
         int second = 9;
 
-        boolean result = first > second;
+        boolean result = first > second; //Почему горит желтым?
         System.out.println(result);
 
         result = first < second; //Не ставлю тип переменной так это значит что я назначаю новую переменную, а я уже ее обьявил на 8 строке?
