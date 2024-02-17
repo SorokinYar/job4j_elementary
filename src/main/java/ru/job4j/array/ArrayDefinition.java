d9dd78f0d2cf9a4e90ff1bc4f4ac1cc2a10b586c
SorokinYar <Sorokin.yar@yandex.ru>
2024-02-17 23:52:18 +0100

    6.0.5. Массивы и цикл for-each [#333581]

---
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -0,0 +1,27 @@
+package ru.job4j.array;
+
+public class ArrayDefinition {
+
+
+    public static void main(String[] args) {
+        short[] ages = new short[10];
+        System.out.println("Размер массива равен: " + ages.length);
+
+        String[] surname = new String[100500];
+        System.out.println("Размер массива равен: " + surname.length);
+
+        float[] prices = new float[40];
+        System.out.println("Размер массива равен: " + prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Petr Arsentev";
+        names[1] = "Dedun Michael";
+        names[2] = "Sorokin Iaroslav";
+        names[3] = "Pavlova Nina";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
+
+    }
+}
